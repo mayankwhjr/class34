aebfa1d2cc274d4163d993b2c3f094898bb79fcc
mayankwhjr <67011206+mayankwhjr@users.noreply.github.com>
2020-08-19 16:51:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var ball;
+var database;
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database();
+    var ballref= database.ref("ball/position")
+    ballref.on("value",readposition)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
+    
+}
+function readposition(data) {
+position=data.val()
+ball.x=position.x;
+ball.y=position.y;
+}
